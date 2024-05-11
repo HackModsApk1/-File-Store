d9abe24ac0cb57c4d9cf7126c072ae30677411a3
HackModsApk1 <161157525+Vaibhav582@users.noreply.github.com>
2024-05-11 22:22:15 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/Advance-File-Share-Bot /Advance-File-Share-Bot
+  git clone https://github.com/💖 𝙷𝚊𝚌𝚔𝙼𝚘𝚍𝚜𝙰𝚙𝚔𝟷 💖/💖 𝙷𝚊𝚌𝚔𝙼𝚘𝚍𝚜𝙰𝚙𝚔𝟷 💖 File Store /💖 𝙷𝚊𝚌𝚔𝙼𝚘𝚍𝚜𝙰𝚙𝚔𝟷 💖 File Store
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Advance-File-Share-Bot
+  git clone $UPSTREAM_REPO /💖 𝙷𝚊𝚌𝚔𝙼𝚘𝚍𝚜𝙰𝚙𝚔𝟷 💖 File Store
 fi
-cd /Advance-File-Share-Bot
+cd /💖 𝙷𝚊𝚌𝚔𝙼𝚘𝚍𝚜𝙰𝚙𝚔𝟷 💖 File Store
 pip3 install -U -r requirements.txt
 echo "Bot Started...."
 python3 bot.py
